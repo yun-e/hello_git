c8cb08bacb38b9d99e51b6733a342a91f826e89f
Seungyun Kim <42404286+yun1026@users.noreply.github.com>
2021-07-11 18:11:51 +0900

    Update Program.cs

--- CSharp/ConsoleApplication/ConsoleApplication/Program.cs
+++ CSharp/ConsoleApplication/ConsoleApplication/Program.cs
@@ -4,6 +4,8 @@ namespace ConsoleApplication {
     internal class Program {
         public static void Main(string[] args) {
             Console.WriteLine("Hello, World!");
+            
+            // Will this change be reflected on my local source code?
         }
     }
 }
